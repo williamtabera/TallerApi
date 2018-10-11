5884c0d054235259660a145ec9b8fb65ed59c4e4
william alberto tabera guerrero <wtabera2010@gmail.com>
2018-10-10 19:46:44 -0500

    Principal

---
+++ TallerApis.Apis/Models/Publicaciones.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TallerApis.Apis.Models
+{
+    public class Publicaciones : DbContext
+    {
+        public Publicaciones() : base("PlublicacionConnection")
+        {
+
+        }
+    }
+}
