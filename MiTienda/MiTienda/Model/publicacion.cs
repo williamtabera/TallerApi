fab0267710554b5d22071cb8f3d4bd33a9a15d51
william alberto tabera guerrero <wtabera2010@gmail.com>
2018-10-15 17:20:50 -0500

    Principal2

---
+++ MiTienda/MiTienda/Model/publicacion.cs
@@ -0,0 +1,20 @@
+﻿using Java.Lang;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiTienda.Model
+{
+    class publicacion
+    {
+        public int Id { get; set; }
+        public string Usuario { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public int Megusta { get; set; }
+        public int MeDisgusta { get; set; }
+        public int VecesCompartido { get; set; }
+        public bool EsPrivada { get; set; }
+
+    }
+}
---
+++ MiTienda/MiTienda/Views/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using MiTienda.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MiTienda
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            CargarApi();
+        }
+
+        private async Task CargarApi()
+        {
+            HttpClient publicaciones = new HttpClient();
+
+            publicaciones.BaseAddress = new Uri("http://192.168.1.51");
+            var request = await publicaciones.GetAsync("MiTiendaApi/api/Publicaciones");
+            if (request.IsSuccessStatusCode)
+            {
+                var responseJson = await request.Content.ReadAsStringAsync();
+                var response = JsonConvert.DeserializeObject<List<publicacion>>(responseJson);
+                listPublicacion.ItemsSource = response;
+            }
+        }
+
+        private async void listPublicaciones(object sender, SelectedItemChangedEventArgs e)
+        {
+
+            var publica = e.SelectedItem as publicacion;
+            string mensaje = string.Format("Usuario : {0} - FechaPublicacion : {1}, Descripcion : {2}", publica.Usuario,publica.FechaPublicacion,publica.Descripcion);
+            await DisplayAlert("Publicacion", mensaje, "Ok");
+        }
+    }
+}
